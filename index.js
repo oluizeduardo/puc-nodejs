187f40748749d847928ec6bc7fecc57cce3a826b
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-06-28 23:13:12 -0300

    Alteração na porta.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var http = require('http');
 var fs = require('fs');
 var content = fs.readFileSync('index.html');
 
-const port = process.env.PORT || 3000;
-
 http.createServer(function(req, res){
 	res.end(content);
-}).listen(port);
+}).listen(3000);
